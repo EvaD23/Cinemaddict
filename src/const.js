bc210161848f7a053cca57b29b0d962c118beb56
Evgeniia Dmitrieva <be230497@gmail.com>
2023-02-14 20:40:17 +0700

    Добавила класс для отправки запросов на сервер

---
+++ src/const.js
@@ -0,0 +1,11 @@
+
+const END_POINT = 'https://19.ecmascript.pages.academy/cinemaddict/';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
+};
+
+export { END_POINT, Method }
